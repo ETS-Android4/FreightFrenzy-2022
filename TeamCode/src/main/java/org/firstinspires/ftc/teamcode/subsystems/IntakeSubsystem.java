1588f9fc2bcdebdecb550cd181a246ac53a35edc
SachetK <71898487+SachetK@users.noreply.github.com>
2021-12-28 12:52:15 -0500

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSubsystem.java
@@ -4,23 +4,25 @@ import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 
 public class IntakeSubsystem extends SubsystemBase {
-    private final Motor intakeMotor;
+    private final Motor intakeL, intakeR;
 
-    public IntakeSubsystem(Motor intakeL){
-        this.intakeMotor = intakeL;
-
-        this.intakeMotor.setInverted(false);
+    public IntakeSubsystem(Motor intakeL, Motor intakeR){
+        this.intakeL = intakeL;
+        this.intakeR = intakeR;
     }
 
     public void in() {
-        intakeMotor.set(1.0);
+        intakeL.set(1.0);
+        intakeR.set(-1.0);
     }
 
     public void out() {
-        intakeMotor.set(-1.0);
+        intakeL.set(-1.0);
+        intakeR.set(1.0);
     }
 
     public void stop() {
-        intakeMotor.stopMotor();
+        intakeL.stopMotor();
+        intakeR.stopMotor();
     }
 }
