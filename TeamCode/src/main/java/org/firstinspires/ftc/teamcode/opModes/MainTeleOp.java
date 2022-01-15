a248713a4561ce494cb7af0e6f3374b20075461e
SachetK <71898487+SachetK@users.noreply.github.com>
2022-01-15 02:48:46 -0500

    Optimize imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/MainTeleOp.java
@@ -3,16 +3,11 @@ package org.firstinspires.ftc.teamcode.opModes;
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
-import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.arcrobotics.ftclib.hardware.motors.CRServo;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.teamcode.commands.arm.ArmExtendCommand;
-import org.firstinspires.ftc.teamcode.commands.arm.ArmOpenLoopCommand;
-import org.firstinspires.ftc.teamcode.commands.arm.LiftOpenLoopCommand;
 import org.firstinspires.ftc.teamcode.commands.arm.test.ArmExtendTestCommand;
 import org.firstinspires.ftc.teamcode.commands.arm.test.ArmRetractTestCommand;
 import org.firstinspires.ftc.teamcode.commands.arm.test.LiftDropTestCommand;
